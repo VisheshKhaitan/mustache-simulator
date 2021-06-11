68943099475f82ec5931ba7c9fade048faa12c27
VisheshKhaitan <78291797+VisheshKhaitan@users.noreply.github.com>
2021-06-11 16:14:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    Canvas = createCanvas(300,300);
+    Canvas.center();
+}
+
+function draw()
+{
+
+}
+
+function take_snapshot()
+{
+    save('close-nose-filter-image.png');
+}
